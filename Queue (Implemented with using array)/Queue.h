5e30e68ddfd45c7c145822a4f3dc70684ed21d16
Emir-Hamza-Arabaci <emr_hmza_arabc@hotmail.com>
2023-02-21 20:33:24 +0300

    Queue Implemented
    A basic queue implemented with using an array.
    Pop,Front,Push functions added.

---
+++ Queue (Implemented with using array)/Queue.h
@@ -0,0 +1,71 @@
+#ifndef QUEUE_HPP
+#include <iostream>
+
+#define QUEUE_HPP
+
+using std::cout;
+using std::cin;
+
+template <typename T, int a>
+class Queue
+{
+	
+public:
+
+		// Constructor and Destructor
+	Queue() : Array(new int[a] {}) , Count(0)
+	{
+	}
+	~Queue()
+	{
+		delete[] Array;
+	}
+
+		// Helper Functions
+	void Print() noexcept
+	{
+		cout << "[";
+		for (int i = 0; i < a; i++)
+			cout << *(Array + i) << ",";
+		cout << "\b]";
+	}
+
+		// Fundemental Functions
+	void Push_Front(T Value)
+	{
+		if (Count == a) throw __cpp_exceptions;
+		Array[Count] = Value;
+
+		Count++;
+	}
+	inline T Front() noexcept
+	{
+		return *Array;
+	}
+	
+	void Pop_Front()
+	{
+		if (Count == 0) throw __cpp_exceptions;
+
+		Shift();
+		Count--;
+	}
+	
+		// Getter Functions
+	inline int count() noexcept { return Count; }
+
+private:
+	int* Array;
+	int Count;
+
+	void Shift() noexcept
+	{
+		for (int i = 1; i < Count; i++)
+			Array[i - 1] = Array[i];
+		
+	}
+
+};
+
+#endif // !QUEUE.HPP
+
---
+++ Queue (Implemented with using array)/Source.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "Queue.h"
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	return 0;
+}
